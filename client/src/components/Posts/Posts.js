fadfdc3b8d6f239250684196229aefa247f06b9f
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-03-30 19:48:58 +0530

    added edit functionality

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles';
 
-const POSTS = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
@@ -19,12 +19,12 @@ const POSTS = () => {
       spacing={3}
     >
       {posts.map((post) => (
-        <Grid key={post.id} item xs={12} sm={6}>
-          <Post post={post} />
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
       ))}
     </Grid>
   );
 };
 
-export default POSTS;
+export default Posts;
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
@@ -28,12 +29,16 @@ export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
 
-  if (mongoose.Types.ObjectId.isValid(_id))
-    return res.status(404).send('No post with that id');
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send(`No post with id`);
 
-  const updatePost = await PostMessage.findByIdAndUpdate(_id, post, {
-    new: true,
-  });
+  const updatePost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  );
 
   res.json(updatePost);
 };
